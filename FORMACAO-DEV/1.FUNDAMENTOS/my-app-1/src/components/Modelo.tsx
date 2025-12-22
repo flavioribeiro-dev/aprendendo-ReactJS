0f24a157bf8e53a5d50bb1959a43cd0cba212344
fribeiro <flavioxconsult@gmail.com>
2025-12-22 20:39:07 -0300

    aulas 25/6 - desafio reorganização

--- FORMACAO-DEV/1.FUNDAMENTOS/my-app-1/src/components/Modelo.tsx
+++ FORMACAO-DEV/1.FUNDAMENTOS/my-app-1/src/components/Modelo.tsx
@@ -1,23 +1,30 @@
+import '@/app/globals.css';
 import Cabecalho from "./Cabecalho";
 import Conteudo from "./Conteudo";
 import Rodape from "./Rodape";
 import Menu from './Menu';
 
-
 export default function PageDefault(props: any) {
     return (
-        <div className="flex flex-col gap-4 h-screen">
-            <Cabecalho
-                cabecalho_titulo={props.cabecalho_titulo}
-            />
-            <div className="flex gap-4 h-screen">
+        <div>
+            <div className={`flex gap-4`}>
                 <Menu />
-                <Conteudo>
-                    <div>{props.children}</div>
-                </Conteudo>
+                <div className="flex flex-col gap-4 h-screen w-screen">
+                    <Cabecalho
+                        cabecalho_titulo={props.cabecalho_titulo}
+                    />
+                    <div className="flex gap-4 h-screen">
+                        
+                        <Conteudo>
+                            <div>{props.children}</div>
+                        </Conteudo>
+                    </div>
+                    
+                    <Rodape />
+                </div>
             </div>
-            
-            <Rodape />
         </div>
+        
+    
     )
 }

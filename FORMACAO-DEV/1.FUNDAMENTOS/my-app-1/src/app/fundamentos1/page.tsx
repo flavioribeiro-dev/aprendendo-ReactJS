1463ce2f81968c576ab5b0dd68ba0b4449682b1e
fribeiro <flavioxconsult@gmail.com>
2025-12-22 09:59:33 -0300

    aula 14 - componente Cabeçalho

---
+++ FORMACAO-DEV/1.FUNDAMENTOS/my-app-1/src/app/fundamentos1/page.tsx
@@ -0,0 +1,14 @@
+import Cabecalho from "@/components/Cabecalho";
+
+
+
+export default function Page() {
+    return (
+        <div className={`
+            p-4
+        `}>
+            <Cabecalho />
+            Criar página na pasta Apps
+        </div>
+    )
+}
---
+++ FORMACAO-DEV/1.FUNDAMENTOS/my-app-1/src/components/Cabecalho.tsx
@@ -0,0 +1,10 @@
+export default function Cabecalho() {
+    return (
+        <div className={`
+            flex justify-center items-center text-3xl
+            bg-purple-500 h-36 rounded-lg
+        `}>
+            Cabeçalho
+        </div>
+    )
+}
